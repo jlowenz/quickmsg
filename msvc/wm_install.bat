698418e13f9ea1d4b7dbda07a0c1dd529985c97b
Ralph Brewer <ralph.w.brewer.civ@mail.mil>
2016-05-12 15:42:55 -0400

    Added maven install script

---
+++ msvc/wm_install.bat
@@ -0,0 +1,2 @@
+call mvn install:install-file -DgroupId=jlowenz -DartifactId=quickmsg -Dversion=1.0 -Dpackaging=jar -Dfile=build/quickmsg.jar
+call mvn install:install-file -DgroupId=jlowenz -DartifactId=quickmsg-native -Dversion=1.0 -Dpackaging=jar -Dfile=build/quickmsg-native.jar
