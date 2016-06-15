f6720a992278b2650a555a4316fc540c3cd14628
Jason Owens <github.jlowenz@xoxy.net>
2016-06-15 08:52:43 -0400

    Renamed wm_install script to install_jars to better reflect behavior

--- msvc/install_jars.bat
+++ msvc/install_jars.bat
@@ -1,2 +1,3 @@
-call mvn install:install-file -DgroupId=jlowenz -DartifactId=quickmsg -Dversion=1.0 -Dpackaging=jar -Dfile=build/quickmsg.jar
-call mvn install:install-file -DgroupId=jlowenz -DartifactId=quickmsg-native -Dversion=1.0 -Dpackaging=jar -Dfile=build/quickmsg-native.jar
+set jar_dir=%1
+call mvn install:install-file -DgroupId=jlowenz -DartifactId=quickmsg -Dversion=1.0 -Dpackaging=jar -Dfile="%jar_dir%\quickmsg.jar"
+call mvn install:install-file -DgroupId=jlowenz -DartifactId=quickmsg-native -Dversion=1.0 -Dpackaging=jar -Dfile="%jar_dir%\quickmsg-native.jar"
